7aa1fa983e3be930106e3434a12577b04ba47dd6
ElsyChavez <00125717@uca.edu.sv>
2018-12-01 15:50:53 -0600

    Se termino de hacer la estructura de archivos como es debida

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res, next){
+  let youtuber = {
+    id: req.paramas.id,
+    nombre: req.body.name,
+    tema: req.body.tema,
+
+  }
+  youtuber.save();
+  res.status(200).json({
+    message: "the posting"
+  })
+});
+
 module.exports = router;
